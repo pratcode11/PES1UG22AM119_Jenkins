a21ed10656a3460c4c24747c2bf83fbf7108fb67
pratcode11 <prarthvader@gmail.com>
2025-03-13 16:07:32 +0530

    Updated hello.cpp with new changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Prarthana :>\n";
-  cout << "new changes made for task 4"
+  cout << "new changes made for task 4";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
